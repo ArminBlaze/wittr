03ab70f392ed30d9615b33ebc7e6fa6a6b65f6b0
ArminBlaze <box_for_spam@mail.ru>
2024-12-24 15:11:57 +0300

    Update index.js

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,6 @@
 self.addEventListener('fetch', (e) => {
 	e.respondWith(
-		new Response('Hello <b>world</b>', {
+		new Response('Hello <b class="a-winner-is-me">world</b>', {
 			headers: { "Content-Type": "text/html" }
 		})
 	);
